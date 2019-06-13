a12929f880960ba1727d3dfba90d34fe2eea9ad1
Christoph <christoph@intanet.de>
2019-06-13 22:21:06 +0200

    git show staged and stashed status

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -29,6 +29,9 @@ function fish_prompt
 				set git_behind (command git rev-list --count $git_branch..$git_remote)
 			end
 			set -l git_dirty (command git status -s 2>/dev/null)
+			set -l git_staged (command git diff --cached 2>/dev/null)
+			set -l git_stashed (command git stash list 2>/dev/null)
+
 			set_color green
 			if test -n "$git_dirty"
 				set_color F70
@@ -37,6 +40,7 @@ function fish_prompt
 			if test -n "$git_branch"
 				echo -n " $git_branch"
 			end
+
 			if test -n "$git_ahead" && test "$git_ahead" -gt 0
 				set_color FC0
 				echo -n " ↑$git_ahead"
@@ -44,6 +48,16 @@ function fish_prompt
 			if test -n "$git_behind" && test "$git_behind" -gt 0
 				set_color F70
 				echo -n " ↓$git_behind"
+			end
+
+			if test -n "$git_staged"
+				set_color BFB
+				echo -n " +"
+			end
+
+			if test -n "$git_stashed"
+				set_color FC0
+				echo -n " ⋮"
 			end
 		end
 	end
