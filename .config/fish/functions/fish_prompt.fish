06e47dd4228366d5b1afa1cb9415985a25bcd9d5
Christoph Schnieder <christoph.schnieder@webxells.com>
2019-06-13 16:28:28 +0200

    show commits ahead and behind

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -11,17 +11,28 @@ function fish_prompt
 	set_color 08F
 	echo -n (hostname)
 	set_color FD6
-	echo -n '' (prompt_pwd) ''
+	echo -n '' (prompt_pwd)
 
 	if type -q git
 		set -l git_dir (command git rev-parse --git-dir 2>/dev/null)
 		if test -n "$git_dir"
+			set -l git_branch (command git branch | sed -n '/\* /s///p' 2>/dev/null)
+			set -l git_remote (command git for-each-ref --format='%(upstream:short)' (command git symbolic-ref -q HEAD) origin/mainline)
+			set -l git_ahead (command git rev-list --count $git_remote..$git_branch)
+			set -l git_behind (command git rev-list --count $git_branch..$git_remote)
 			set_color green
-			#echo -n '' (command git branch | grep \* | cut -d ' ' -f2- 2>/dev/null) ''
-			echo -n '' (command git branch | sed -n '/\* /s///p' 2>/dev/null) ''
+			echo -n "  $git_branch"
+			if test "$git_ahead" -gt 0
+				set_color FC0
+				echo -n " ↑$git_ahead"
+			end
+			if test "$git_behind" -gt 0
+				set_color F70
+				echo -n " ↓$git_behind"
+			end
 		end
 	end
 
 	set_color normal
-	echo '⋟ '
+	echo ' ⋟ '
 end
