fe0773fe8d4f90983d869721d2df747ee8898ac2
Christoph Schnieder <christoph.schnieder@webxells.com>
2020-03-26 14:25:33 +0100

    added simple switch and length

--- .config/fish/functions/pwgen.fish
+++ .config/fish/functions/pwgen.fish
@@ -1,3 +1,14 @@
-function pwgen
-	cat /dev/urandom | tr -dc 'a-zA-Z0-9!#$%&()*+,-./:;<=>?@[]^_{|}~' | fold -w 32 | head -n 1
+function pwgen --description 'Generate a password'
+	if contains -- --simple $argv
+		set chars 'a-zA-Z0-9'
+	else
+		set chars 'a-zA-Z0-9!#$%&()*+,-./:;<=>?@[]^_{|}~'
+	end
+	if string match --quiet --regex --invert '\D' -- $argv[-1]
+		set length $argv[-1]
+	else
+		set length 32
+	end
+	cat /dev/urandom | tr -dc $chars | fold -w $length | head -n 1
+	#cat /dev/urandom | tr -dc 'a-zA-Z0-9!#$%&()*+,-./:;<=>?@[]^_{|}~' | fold -w 32 | head -n 1
 end
