f83ceb3ee01888cc72e45a797755094d7f446a97
Christoph Schnieder <christoph.schnieder@webxells.com>
2020-04-17 17:43:33 +0200

    hmpf, header case...

--- .tools/install-rocketchat.sh
+++ .tools/install-rocketchat.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 release_url="https://github.com/RocketChat/Rocket.Chat.Electron/releases/"
-current_url=$(curl -I ${release_url}latest | grep -oP '(?<=^Location: )[\w\d:/.]+')
+current_url=$(curl -I ${release_url}latest | grep -oP '(?<=^location: )[\w\d:/.]+')
 current_version=$(echo "$current_url" | grep -oP '(?<=/tag/)[0-9.]+')
 deb_name=rocketchat_${current_version}_amd64.deb
 download_url=${release_url}download/${current_version}/${deb_name}
