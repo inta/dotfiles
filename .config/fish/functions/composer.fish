f834092380fafaeec560de0d7f332db0ccb6489a
pcs <christoph.schnieder@webxells.com>
2022-12-16 21:12:28 +0100

    vscode settings updated

--- .config/fish/functions/composer.fish
+++ .config/fish/functions/composer.fish
@@ -1,3 +1,3 @@
 function composer --description 'getcomposer.org'
-	docker run --rm -it --volume (pwd):/app --user (id -u):(id -g) composer --ignore-platform-reqs $argv
+	docker run --rm -it --volume (pwd):/app --user (id -u):(id -g) nexus.webxells.com/composer --ignore-platform-reqs $argv
 end
