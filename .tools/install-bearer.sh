fb9688b67bb41c45acb9a975df6db0d7f0fd3578
pcs <christoph.schnieder@webxells.com>
2023-10-27 09:37:10 +0200

    bearer install added

--- .tools/install-bearer.sh
+++ .tools/install-bearer.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-release_url=https://github.com/wagoodman/dive/releases/
-release_version=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-deb_name=dive_${release_version}_linux_amd64.deb
+release_url=https://github.com/Bearer/bearer/releases/
+release_version=$(curl -sL "https://api.github.com/repos/Bearer/bearer/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+deb_name=bearer_${release_version}_linux-amd64.deb
 download_url=${release_url}download/v${release_version}/${deb_name}
 deb_file=/tmp/${deb_name}
 current_version=unknown
 
-if command -v dive >/dev/null; then
-	current_version=$(dive version | grep -oP '\d[\d.]+\d')
+if command -v bearer >/dev/null; then
+	current_version=$(bearer version 2>&1 | head -n 1 | grep -oP '\d[\d.]+\d')
 fi
 
 if [ "$current_version" = "$release_version" ]; then
@@ -22,13 +22,13 @@ if [ "$(id -u)" -ne 0 ]; then
       exec su - -c "$0 $*"
 fi
 
-echo "Downloading dive ..."
+echo "Downloading bearer ..."
 if ! curl -fsSL "$download_url" -o "$deb_file"; then
 	echo "Download failed"
 	exit 1
 fi
 
-echo "Installing dive ${release_version} ..."
+echo "Installing bearer ${release_version} ..."
 if dpkg -i "$deb_file"; then
 	rm "$deb_file"
 	echo "DONE"
