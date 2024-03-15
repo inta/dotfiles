4d033a7bd0435301bc63c3d5ac67b5767772e4bd
pcs <christoph.schnieder@webxells.com>
2024-03-15 15:00:47 +0100

    kmonad added

---
+++ .tools/install-kmonad.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+release_version=$(curl -fsSI 'https://github.com/kmonad/kmonad/releases/latest' | grep -i 'location:' | grep -oP '(?<=/)[\d.]+')
+file_name="kmonad"
+download_url="https://github.com/kmonad/kmonad/releases/download/$release_version/$file_name"
+dest_dir=$HOME/.local/bin
+
+if [ ! -d "$dest_dir" ]; then
+	echo "Aborting, $dest_dir is not a directory!"
+	exit
+fi
+
+if command -v kmonad >/dev/null && [ "$(kmonad --version | grep -oP '^[\d.]+')" = "$release_version" ]; then
+	echo "Alreasy up to date..."
+	exit
+fi
+
+echo "Kmonad $release_version will be downloaded now …"
+curl -Lo "$dest_dir/$file_name" "$download_url"
+chmod +x "$dest_dir/$file_name"
